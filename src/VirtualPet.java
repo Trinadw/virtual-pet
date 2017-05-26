170bbb79002fc1d9becb69193a1701b0375d6fbc
Trina <triwil74@yahoo.com>
2017-05-26 14:41:16 -0400

    pet monkey

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,4 +1,53 @@
 
 public class VirtualPet {
+	String name;
+	int hunger = 7;
+	int thirst = 4;
+	int energy = 9;
 
+	
+	
+	
+
+int getHunger(){
+	return hunger;
 }
+
+boolean isHungry(){
+	return hunger>=7;
+	}
+int getThirst(){
+	return thirst;
+}
+boolean isThirsty(){
+	return thirst>=4;
+}
+	int getEnergy(){
+		return energy;
+			}
+	boolean isTired(){
+		return energy<=7;
+			}
+	void feed(){
+	hunger = hunger-2;
+	thirst = thirst +1;
+	energy = energy -3;
+			}
+	void water(){
+		thirst = thirst-1;
+		energy = energy -1;
+	}
+	void bed(){
+		energy = energy +3;
+		hunger = hunger +3;
+	}
+	void tick(){
+		hunger = hunger + 2;
+		thirst = thirst + 3;
+		energy = energy -4;
+				
+	}
+	
+}
+
+
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -1,8 +1,49 @@
+import java.util.Scanner;
 
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		VirtualPet jax = new VirtualPet();
+		jax.name = "Jax";
+		writeLine("My pet monkey's name is " + jax.name);
+		System.out.println("Jax is currently feeling on a scale of 1-20, hunger =  " + jax.getHunger() + " ,thirst = "
+				+ jax.getThirst() + " and energy = " + jax.getEnergy() + ".");
+
+		do{
+		System.out.println("What would you like to do: feed, water, or put Jax to bed?");
+		String state = input.next();
+		switch (state) {
+		case "feed":
+			jax.feed();
+			break;
+		case "water":
+			jax.water();
+			break;
+		case "bed":
+			jax.bed();
+			break;
+		case "Quit":
+			System.out.println("Don't be a quitter! Think of Jax and how she feels.");
+				System.exit(0);
+		
+		}
+		System.out.println("Jax is currently feeling hunger =  " + jax.getHunger() + " ,thirst = "
+				+ jax.getThirst() + " and energy = " + jax.getEnergy() + ".");
+		jax.tick();
+		System.out.println("Go enjoy yourself for a couple hours.");
+		
+		System.out.println("Three hours later, Jax is currently feeling hunger =  " + jax.getHunger() + " ,thirst = "
+				+ jax.getThirst() + " and energy = " + jax.getEnergy() + ".");
+		}
+		while(jax.getEnergy()>0);
+		if (jax.getEnergy()<=0){
+			System.out.println("Sorry, Jax is no longer with us. You'll need to dig a hole in the backyard.");
+		}
+	}
+
+	public static void writeLine(String message) {
+		System.out.println(message);
 
 	}
 
